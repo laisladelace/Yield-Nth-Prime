89e4a03e7b4f0d9366893dfef218464c28dfc094
laisladelace <155329225+laisladelace@users.noreply.github.com>
2024-01-16 16:49:23 -0800

    Update YieldNthPrime.py

--- YieldNthPrime.py
+++ YieldNthPrime.py
@@ -1,55 +1,24 @@
 def is_prime(n):
-    if n ==1: 
+    if n==1: 
         return False 
     for i in range (2,n):
-        if n%i==0:
-            return False
+        if n %i==0:
+            return False 
     else: return True 
 
 def yield_nth_prime():
-    n = int (input("Enter n:"))
-    _zero= 0 
-    _two = 2 
-    while _zero < n: 
+    n = int(input("Enter n:"))
+#Initialize a "counting" variable called _zero to the number 0 and a variable _two to 2 (the first prime number)
+    _zero = 0 
+    _two = 2
+#Enter a while loop to check if _two is prime using the is_prime function based on _zero's value. 
+##If _two IS prime, _zero increases by 1.
+###If _zero equals n, then _two is the nth prime number. 
+####If _two does not equal n, and it is not prime, _zero increases again by 1 until we find the next prime number. 
+    while _zero<n: 
         if is_prime(_two):
-            _zero +=1
-        if _zero ==n:
-            return _two
-        _two+=1
-
-
-print (yield_nth_prime(7))
-
-
-
-
-#Just in case you didn't get the invite for the other repository: 
-
-import time 
-start = time.time()
-
-
-def find_a_list_of_prime_numbers():
-
-    print ('Choose a range (from x through y)')
-    x_value = int(input('Enter x(greater than 1):'))
-    y_value = int(input('Enter y:'))
-
-    list_prime_num = []
-    for number in range(x_value, y_value +1):
-        for n in range (2, number):
-            if number %n==0:
-                break 
-            else: 
-                list_prime_num.append(number)
-    print()
-    print(f'Prime numbers between {x_value} and {y_value} are:')
-    for prime in list_prime_num:
-        print(f'{prime}', end = '')
-    print()
-
-find_a_list_of_prime_numbers()
-
-end = time.time()
-print (f"The total run time for the program was:")
-print(round(end-start,2)),"seconds")
+            _zero +=1 
+        if _zero ==n: 
+            return _two 
+        _two +=1 
+print (yield_nth_prime())
